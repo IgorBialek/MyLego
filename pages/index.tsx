50968e0213da2eaf6028627aac89daa3e0b9b6a4
IgorBialek <igorbialek0@gmail.com>
2022-11-08 21:51:06 +0100

    Update Logged User Rediraction

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,27 @@
-import type { NextPage } from "next";
+import type { GetServerSideProps, NextPage } from "next";
+import { getSession } from 'next-auth/react';
+
 import Home from '../components/home/Home';
 
 const HomePage: NextPage = () => {
   return <Home />;
+};
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const session = await getSession(context);
+
+  if (session) {
+    return {
+      redirect: {
+        destination: "/dashboard",
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {},
+  };
 };
 
 export default HomePage;
